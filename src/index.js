d84cb0f4dc2e57cff2c0a07337d2a6ec49519339
joshborup <joshborup@gmail.com>
2018-10-17 16:04:20 -0700

    attempted pwa added react snap and serviceworker

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { hydrate, render } from "react-dom";
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+registerServiceWorker();
