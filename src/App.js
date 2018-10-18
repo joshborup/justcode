067cbfe333673c29cae36368e5f526e82fbcc7d4
joshborup <joshborup@gmail.com>
2018-10-17 22:14:07 -0700

    pushing build

--- src/App.js
+++ src/App.js
@@ -89,7 +89,26 @@ class App extends Component {
   }
 
   componentDidMount(){
-    
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can add to home screen
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
 
     // window.addEventListener('resize', ()=> this.throttle(console.log(window.innerHeight), 75))
 
