39a23a5bfba9144777bfd570b7b493864902b201
joshborup <joshborup@gmail.com>
2018-10-18 13:09:38 -0700

    updated accessibility

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import JSZip from 'jszip';
-import FileSaver from 'file-saver';
 
 export default class ToolBar extends Component {
     state = {
@@ -12,8 +10,6 @@ export default class ToolBar extends Component {
        const {language:l} = this.props;
        console.log('this is just the l variable', l)
        let downloadable = this.props.code ? this.props.code : this.props.code;
-       let extension = l === 'javascript' ? 'js' : l === 'html' ? 'html' : l === 'css' ? 'css' : l === 'scss' ? 'scss' : l === 'sql' ? 'sql' : l === 'typescript' ? 'ts' : l === 'markdown' ? 'markdown' : l === 'text' ? 'txt' : null;
-       
 
         return (
             <div className={this.props.drawerToggle ? 'toolbar-container' : 'toolbar-container hide'}>
@@ -30,27 +26,20 @@ export default class ToolBar extends Component {
                         
                         
                     }}>{this.state.clipboardLabel}</a>
-                    {/* <div className='pane-selection-container'>
-                        <label>Notes Tab: </label>
-                        <select name='split' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.split}>
-                            <option value='1'>hide</option>
-                            <option value='2'>show</option>
-                        </select>
-                    </div> */}
                     <div className='instructions-container'>
                         <h2>How it works:</h2>
                         <p>Enter a name and hit <span>"Go"</span> to join or create a room, anyone in this same room will be able to collaborate in realtime</p>
 
                     </div>
                     <div className='room-selection-container container'>
-                        <label>Room: </label>
-                        <input name='room' onChange={(e) => this.props.roomChangeHandler(e.target.name, e.target.value)} value={this.props.room} />
+                        <label for='room'>Room: </label>
+                        <input name='room' id='room' onChange={(e) => this.props.roomChangeHandler(e.target.name, e.target.value)} value={this.props.room} />
                         <button onClick={() => window.location.pathname = this.props.room}>Go</button>
                     </div>
 
                     <div className='language-selection-container container'>
-                        <label>Language: </label>
-                        <select name='language' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.language}>
+                        <label for='language'>Language: </label>
+                        <select name='language' id='language' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.language}>
                             <option value='javascript'>JavaScript</option>
                             <option value='html'>HTML</option>
                             <option value='css'>CSS</option>
@@ -69,8 +58,8 @@ export default class ToolBar extends Component {
                     </div>
 
                     <div className='language-selection-container container'>
-                        <label>Theme: </label>
-                        <select name='theme' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.theme}>
+                        <label for='theme'>Theme: </label>
+                        <select name='theme' id='theme' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.theme}>
                             <option value='monokai'>Monokai</option>
                             <option value='github'>Github</option>
                             <option value='terminal'>terminal</option>
@@ -83,23 +72,23 @@ export default class ToolBar extends Component {
                     </div>
 
                     <div className='font-size-container container'>
-                        <label>Font Size: </label>
-                        <input type='number' name='fontSize' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.fontSize} />
-                        <label>tabSize: </label>
-                        <input type='number' name='tabSize' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.tabSize} />
+                        <label for='fontsize'>Font Size: </label>
+                        <input id='fontsize' type='number' name='fontSize' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.fontSize} />
+                        <label for='tabSize'>tabSize: </label>
+                        <input id='tabSize' type='number' name='tabSize' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.tabSize} />
                     </div>
 
                     <div className='wordWrap-container container'>
-                        <label>Word Wrap: </label>
-                        <select name='wordWrap' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.wordWrap}>
+                        <label for='wordWrap'>Word Wrap: </label>
+                        <select id='wordWrap' name='wordWrap' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.wordWrap}>
                             <option value={false}>No-Wrap</option>
                             <option value={true}>Wrap</option>
                         </select>
                     </div>
 
                     <div className='wordWrap-container container'>
-                        <label>Auto Complete: </label>
-                        <select name='enableLiveAutocompletion' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.enableLiveAutocompletion}>
+                        <label for='autoComplete'>Auto Complete: </label>
+                        <select id='autoComplete' name='enableLiveAutocompletion' onChange={(e) => this.props.changeHandler(e.target.name, e.target.value)} value={this.props.enableLiveAutocompletion}>
                             <option value={false}>Disabled</option>
                             <option value={true}>Enabled</option>
                         </select>
